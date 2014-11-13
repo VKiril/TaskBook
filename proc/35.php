46f44fe6af50e6f7bff4ebbbd5317e6a65500dc4
asus <everest0corp@gmail.com>
2014-11-13 19:33:43 +0200

    the 35'th exercise is done

--- proc/35.php
+++ proc/35.php
@@ -1,7 +1,33 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: asus
- * Date: 13.11.14
- * Time: 18:14
- */ 
+ * Write a real-valued function Fact2(N) that returns a double factorial N!!:
+ * N!! = 1·3·5·…·N,    if N is an odd number;
+ * N!! = 2·4·6·…·N    otherwise
+ * (N is a positive integer; the real return type allows to avoid the integer
+ * overflow during the calculation of the double factorials for large values of N).
+ * Using this function, find the double factorials of five given integers.
+ */
+
+function Fact2($n){
+    if($n % 2 == 0){
+        $result = func($n);
+        $result  = 'even  '.$result;
+    } else {
+        $result = func($n);
+        $result = 'odd &nbsp; '.$result;
+    }
+
+    return $result ;
+}
+
+function func($n){
+    $factorial = 1 ;
+    for($i = $n ; $i > 0 ; $i-=2){
+        $factorial = $factorial* $i;
+    }
+    return $factorial;
+}
+echo(Fact2(6)).'<br/>';
+echo(Fact2(5)) .'<br/>';
+echo(Fact2(3)).'<br/>';
+echo(Fact2(4)).'<br/>';
