657896b51212143bc6baccba56d738af29046f06
asus <everest0corp@gmail.com>
2014-11-13 16:41:15 +0200

    the 28'th exercise is done

--- proc/29.php
+++ proc/29.php
@@ -1,7 +1,16 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: asus
- * Date: 13.11.14
- * Time: 16:24
- */ 
+ * Write an integer function DigitCount(K) that returns the amount of
+ * digits in the decimal representation of a positive integer K. Using
+ * this function, find the amount of digits for each of five given positive
+ * integers.
+ */
+
+function DigitCount($K){
+    return strlen($K);
+
+}
+echo '<br/> amount of digits in number 123545654 is : '.(DigitCount(123545654));
+echo '<br/> amount of digits in number 12354456765654 is : '.(DigitCount(12354456765654));
+echo '<br/> amount of digits in number 12334677654545654 is : '.(DigitCount(12334677654545654));
+echo '<br/> amount of digits in number 125654 is : '.(DigitCount(125654));
