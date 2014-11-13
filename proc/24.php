9cfd815086b8bd008ad220a7798ed29861063d8f
asus <everest0corp@gmail.com>
2014-11-13 15:41:45 +0200

    the 24'th exercise is done

--- proc/24.php
+++ proc/24.php
@@ -1,7 +1,19 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: asus
- * Date: 13.11.14
- * Time: 16:23
- */ 
+ * Write a logical function Even(K) that returns True, if an integer parameter K
+ * is an even number, and False otherwise. Using this function, find the amount of
+ * even numbers in a given sequence of 10 integers.
+ */
+
+function Even($K){
+    if($K %2 == 0){
+
+        return 'true';
+    }else{
+
+        return 'false';
+    }
+}
+
+echo 'the given number (10) is even or no : '.(Even(10)).'<br/>';
+echo 'the given number (11) is even or no : '.(Even(11)).'<br/>';
