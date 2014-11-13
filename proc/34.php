d0f3bc9b37348b0a3fd7dbddcf5eb6e7989d02f4
asus <everest0corp@gmail.com>
2014-11-13 19:08:11 +0200

    the 34'th exercise is done

--- proc/34.php
+++ proc/34.php
@@ -1,7 +1,17 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: asus
- * Date: 13.11.14
- * Time: 18:14
- */ 
+ * Write a real-valued function Fact(N) that returns a factorial
+ * of a positive integer N: N! = 1·2·…·N (the real return type
+ * allows to avoid the integer overflow during the calculation of
+ * the factorials for large values of the parameter N). Using this
+ * function, find the factorials of five given integers.
+ */
+
+function Fact($n){
+
+    return $n?$n*Fact($n-1):1;
+}
+echo(Fact(10)).'<br/>';
+echo(Fact(5)) .'<br/>';
+echo(Fact(14)).'<br/>';
+echo(Fact(20)).'<br/>';
