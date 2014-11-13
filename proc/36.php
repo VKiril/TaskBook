26990bcbc50e0f13a1a4f8c1832a6d1688148248
asus <everest0corp@gmail.com>
2014-11-13 19:47:22 +0200

    the 356'th exercise is done

--- proc/36.php
+++ proc/36.php
@@ -1,7 +1,23 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: asus
- * Date: 13.11.14
- * Time: 18:14
- */ 
+ * Write an integer function Fib(N) that returns the value of N-th term of the sequence of the
+ * Fibonacci numbers. The Fibonacci numbers FK are defined as follows:
+ * F1 = 1,        F2 = 1,        FK = FK−2 + FK−1,    K = 3, 4, … .
+ * sing this function, find five Fibonacci numbers with given order numbers N1, N2, …, N5.
+ */
+function Fib($N){
+    $x = 0 ;
+    $y = 1 ;
+    $z = 0 ;
+
+    while($z < $N) {
+
+        $z = $x + $y;
+        echo($z."<br />");
+        $x = $y;
+        $y = $z;
+    }
+}
+
+
+Fib(50);
