26ee90ff2826ca6feb9d52c39daf83adc09c6d8f
asus <everest0corp@gmail.com>
2014-11-13 18:32:34 +0200

    the 32'th exercise is done

--- proc/32.php
+++ proc/32.php
@@ -1,7 +1,22 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: asus
- * Date: 13.11.14
- * Time: 18:13
- */ 
+ * Write a real-valued function DegToRad(D) that converts the
+ * angle value D in degrees into the one in radians (D is a real
+ * number, 0 ≤ D < 360). Note that 180° = π radians and use 3.14
+ * for a value of π. Using this function, convert five given angles
+ * from degrees into radians
+ */
+
+const degree = 0.0174532925;
+
+function DegToRad($D){
+    return ($D * degree);
+}
+$numb = 180 ;
+echo('number of degrees is : '.$numb . ' and this is '.(DegToRad($numb).'radians')).'<br/>';
+
+$numb = 360 ;
+echo('number of degrees is : '.$numb . ' and this is '.(DegToRad($numb).'radians')).'<br/>';
+
+$numb = 90 ;
+echo('number of degrees is : '.$numb . ' and this is '.(DegToRad($numb).'radians')).'<br/>';
