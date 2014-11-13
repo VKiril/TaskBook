a05ae9a3dac950dd70d4c8d1f0ad44b5b28576a9
asus <everest0corp@gmail.com>
2014-11-13 15:57:17 +0200

    the 25'th exercise is done

--- proc/25.php
+++ proc/25.php
@@ -1,7 +1,27 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: asus
- * Date: 13.11.14
- * Time: 16:23
- */ 
+ * Write a logical function IsSquare(K) that returns True,
+ * if an positive integer parameter K is a square of some integer,
+ * and False otherwise. Using this function, find the amount of squares
+ * in a given sequence of 10 positive integers.
+ */
+
+function IsSquare($K){
+    $request = '';
+    for ($i = 1 ; $i< $K ; $i++){
+        if(pow($i , 2) == $K){
+            $request = 'true';
+            return $request;
+        }else{
+            $request = 'false';
+        }
+
+    }
+    return $request;
+}
+$var1 = 144 ;
+echo 'the given number is : '.$var1.' is this number of a square of some integer ? '.(IsSquare($var1)).'<br/>';
+$var = 169 ;
+echo 'the given number is : '.$var.' is this number of a square of some integer ? '.(IsSquare($var)).'<br/>';
+$var2 = 200 ;
+echo 'the given number is : '.$var2.' is this number of a square of some integer ? '.(IsSquare($var2)).'<br/>';
