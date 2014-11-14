04f6776f6a6789e73bafb24eb0d3bfbad839bd86
asus <everest0corp@gmail.com>
2014-11-14 12:30:13 +0200

    the 38'th exercise is done

--- proc/38.php
+++ proc/38.php
@@ -1,7 +1,29 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: asus
- * Date: 14.11.14
- * Time: 12:41
- */ 
+ * Write a real-valued function Power2(A, N) that returns the power AN calculated by the following formulas:
+ * A0 = 1;
+ * AN = A·A·…·A    (N factors),    if N > 0;
+ * AN = 1/(A·A·…·A)    (|N| factors),    if N < 0
+ * (A is a real-valued parameter, N is an integer parameter). Having input a real
+ * number A and integers K, L, M and using this function, find the powers AK, AL, AM.
+ */
+function Power2($A, $N){
+    $AN = 0 ;
+    if($N == 0 ){
+        $AN = 1 ;
+    }elseif ($N > 0 ){
+        $AN = pow($A , $N);
+    } else {
+        $AN = pow($A , abs($N));
+        $AN = 1 / $AN ;
+    }
+    return $AN ;
+}
+
+$A = 3 ;
+$K = 5 ;
+$L = 0 ;
+$M = -2 ;
+echo 'the power AN is '.(Power2($A , $K)).' when A is '.$A.' and N is '.$K.'<br/>';
+echo 'the power AN is '.(Power2($A , $L)).' when A is '.$A.' and N is '.$L.'<br/>';
+echo 'the power AN is '.(Power2($A , $M)).' when A is '.$A.' and N is '.$M.'<br/>';
--- proc/39.php
+++ proc/39.php
@@ -1,7 +1,33 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: asus
- * Date: 14.11.14
- * Time: 12:41
- */ 
+ * Using the Power1 and Power2 functions (see Proc37 and Proc38),
+ * write a real-valued function Power3(A, B) that returns the power
+ * AB calculated as follows (A and B are real-valued parameters): if
+ * B has a zero-valued fractional part then the function Power2(A, N)
+ * is called (an integer variable N is equal to B), otherwise the function
+ * Power1(A, B) is called. Having input real numbers P, A, B, C and using the
+ * Power3 function, find the powers AP, BP, CP.
+ */
+
+
+include '38.php';
+include '37.php';
+
+function Power3($A, $B){
+    $var = ceil ($B);
+    if($var === $B){
+        $result = Power1($A , $B);
+    } else {
+        $result = Power2($A , $B);
+    }
+
+    return $result;
+}
+
+$A = 3 ;
+$K = 5.2 ;
+$L = 0 ;
+$M = 2 ;
+echo 'the power AN is '.(Power2($A , $K)).' when A is '.$A.' and N is '.$K.'<br/>';
+echo 'the power AN is '.(Power2($A , $L)).' when A is '.$A.' and N is '.$L.'<br/>';
+echo 'the power AN is '.(Power2($A , $M)).' when A is '.$A.' and N is '.$M.'<br/>';
