38552ea14038a45f1d5c973d7d7b32e61a07f86c
asus <everest0corp@gmail.com>
2014-11-17 19:07:23 +0200

    the third exercise is done for javascript chapter

--- func/3.php
+++ func/3.php
@@ -1,7 +1,17 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: asus
- * Date: 17.11.14
- * Time: 19:41
- */ 
+ * Write a real-valued function CircleS(R) that returns the area of a circle
+ * of radius R (R is a real number). Using this function, find the areas of
+ * three circles of given radiuses. Note that the area of a circle of radius
+ * R can be found by formula S = π·R2. Use 3.14 for a value of π.
+ */
+?>
+<p id="demo"></p>
+<script>
+    const pi = 3.14;
+    function CircleS(R){
+        var area = pi* Math.pow(R , 2);
+        return area ;
+    }
+    document.getElementById('demo').innerHTML = 'circle area is '+CircleS(9);
+</script>
