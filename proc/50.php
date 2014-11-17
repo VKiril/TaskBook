76762fd9ae3f46a9cb242d221db84082fd2c473a
asus <everest0corp@gmail.com>
2014-11-17 12:44:02 +0200

    the 50'th exercise is done

--- proc/50.php
+++ proc/50.php
@@ -1,7 +1,35 @@
+
+<head>
+    <title>50</title>
+</head>
 <?php
 /**
- * Created by PhpStorm.
- * User: asus
- * Date: 14.11.14
- * Time: 13:53
- */ 
+ * Write a procedure TimeToHMS(T, H, M, S) that converts a time interval T
+ * (in seconds) into the "hours H, minutes M, seconds S" format
+ * (T is an input integer parameter, H, M, S are output integer parameters).
+ * Using this procedure, find the amount of hours, minutes and seconds for each
+ * of five given time intervals T1, T2, …, T5.
+ */
+
+function TimeToHMS($T/*, &$H, &$M, &$S*/){
+    $result = array(
+        'seconds' => 0 ,
+        'minutes' => 0 ,
+        'hours'   => 0
+    );
+    if ($T >= 60){
+        $result['minutes'] = intval($T / 60) ;
+        $result['seconds'] = $T - ($result['minutes'] * 60);
+        if($result['minutes'] >=  60){
+            $result['hours'] = intval($result['minutes'] / 60);
+            $result['minutes'] = $result['minutes']- ($result['hours'] * 60);
+        }
+    }
+    return $result;
+}
+
+print_r(TimeToHMS(3700));echo('<br/>');
+print_r(TimeToHMS(3600));echo('<br/>');
+print_r(TimeToHMS(3699));echo('<br/>');
+print_r(TimeToHMS(123));echo('<br/>');
+
