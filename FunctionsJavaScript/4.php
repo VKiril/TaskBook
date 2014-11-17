e87b9a5a40c8117c535dab725be2efea69bcb82b
asus <everest0corp@gmail.com>
2014-11-17 19:56:48 +0200

    the 5'th exercise is done

--- FunctionsJavaScript/4.php
+++ FunctionsJavaScript/4.php
@@ -1,7 +1,25 @@
+<head>
+    <title>4</title>
+</head>
 <?php
 /**
- * Created by PhpStorm.
- * User: asus
- * Date: 17.11.14
- * Time: 19:42
- */ 
+ * Write a real-valued function RingS(R1, R2) that returns the area of a
+ * ring bounded by a concentric circles of radiuses R1 and R2 (R1 and R2 are real
+ * numbers, R1 > R2). Using this function, find the areas of three rings with given outer
+ * and inner radiuses. Note that the area of a circle of radius R can be found by formula
+ * S = π·R2. Use 3.14 for a value of π.
+ */
+?>
+<div id="placeholder"></div>
+<script>
+    const  pi = 3.14 ;
+    function RingS(R1, R2){
+        var s1 = (Math.pow(R1 , 2) * pi) ;
+        var s2 = (Math.pow(R2 , 2) * pi) ;
+        console.log('s1 is ' + s1 + ' and s2 is' + s2);
+        var ringArea = Math.abs(s2 - s1);
+        ringArea = ringArea.toFixed(3);
+        return ringArea ;
+    }
+    document.getElementById('placeholder').innerHTML = RingS(4 , 6);
+</script>
--- FunctionsJavaScript/5.php
+++ FunctionsJavaScript/5.php
@@ -1,7 +1,28 @@
+<head>
+    <title>5</title>
+</head>
+
 <?php
 /**
- * Created by PhpStorm.
- * User: asus
- * Date: 17.11.14
- * Time: 19:42
- */ 
+ * Write a real-valued function TriangleP(a, h) that returns the perimeter of an
+ * isosceles triangle with given base a and altitude h (a and h are real numbers).
+ * Using this function, find the perimeters of three triangles with given bases and
+ * altitudes. Note that the leg b of an isosceles triangle can be found by the
+ * Pythagorean theorem:
+ * b2 = (a/2)2 + h2.
+ */
+?>
+<div id="body"></div>
+<script>
+    function TriangleP(a, h){
+        var a2   = Math.pow((a/2) , 2);
+        var h2   = Math.pow(h , 2);
+        var root = Math.sqrt((a2+h2));
+        var perimeter = ( 2 * root ) + a ;
+        perimeter = perimeter.toFixed(3);
+
+        return perimeter;
+    }
+    document.getElementById('body').innerHTML = 'perimeter of this triangle is : '+TriangleP(4 , 6);
+
+</script>
