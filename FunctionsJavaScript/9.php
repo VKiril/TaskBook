4f1dbfb6486cfbe5cbda50e76239007bcf0c7cc2
asus <everest0corp@gmail.com>
2014-11-17 20:48:37 +0200

    the 9'th exercise is done

--- FunctionsJavaScript/9.php
+++ FunctionsJavaScript/9.php
@@ -1,7 +1,35 @@
+<head>
+    <title>8</title>
+</head>
 <?php
 /**
- * Created by PhpStorm.
- * User: asus
- * Date: 17.11.14
- * Time: 21:11
- */ 
+ * Write a logical function Even(K) that returns True, if an integer
+ * parameter K is an even number, and False otherwise. Using this function,
+ * find the amount of even numbers in a given sequence of 10 integers.
+ */
+
+?>
+<div id="body"></div>
+<div id="body1"></div>
+<div id="body2"></div>
+<div id="body3"></div>
+<div id="body4"></div>
+<div id="body5"></div>
+<script>
+    function Even(K){
+        var response = true ;
+        if(K % 2 == 0){
+            response = true ;
+        } else {
+            response = false ;
+        }
+
+        return response ;
+    }
+    document.getElementById('body').innerHTML = 'is number 9 even ? ' + Even(9) ;
+    document.getElementById('body1').innerHTML = 'is number 10 even ? ' + Even(10) ;
+    document.getElementById('body2').innerHTML = 'is number 2 even ? ' + Even(2) ;
+    document.getElementById('body3').innerHTML = 'is number 3 even ? ' + Even(3) ;
+    document.getElementById('body4').innerHTML = 'is number 11 even ? ' + Even(11) ;
+    document.getElementById('body5').innerHTML = 'is number 16 even ? ' + Even(16) ;
+</script>
