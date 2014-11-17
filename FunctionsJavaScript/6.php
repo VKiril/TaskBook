c289db43edf188b25eaa76c4bb02d7e6b3f4e51b
asus <everest0corp@gmail.com>
2014-11-17 20:09:28 +0200

    the 6'th exercise is done

--- FunctionsJavaScript/6.php
+++ FunctionsJavaScript/6.php
@@ -1,7 +1,27 @@
+<head>
+    <title>5</title>
+</head>
 <?php
 /**
- * Created by PhpStorm.
- * User: asus
- * Date: 17.11.14
- * Time: 19:42
- */ 
+ * Write an integer function SumRange(A, B) that returns a sum of
+ * all integers in the range A to B inclusively (A and B are integers).
+ * In the case of A > B the function returns 0. Using this function, find a
+ * sum of all integers in the range A to B and in the range B to C provided
+ * that integers A, B, C are given.
+ */
+
+?>
+<div id="body"></div>
+<script>
+    function SumRange(A, B){
+        var i   = 0;
+        var sum = 0 ;
+        for(i = A ; i <= B ; i ++){
+            sum += i;
+        }
+        return sum;
+    }
+
+    document.getElementById('body').innerHTML = 'the sum of all numbers between 0 and 100 is ' + SumRange(0 , 100);
+
+</script>
