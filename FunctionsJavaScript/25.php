b7c16f37dc6e4a631d49038465a107bb7efbd912
asus <everest0corp@gmail.com>
2014-11-18 12:59:06 +0200

    the 25'th exercise is done

--- FunctionsJavaScript/25.php
+++ FunctionsJavaScript/25.php
@@ -1,7 +1,26 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: asus
- * Date: 18.11.14
- * Time: 13:06
- */ 
+ * Write a function TrianglePS(a) that computes the perimeter
+ * P = 3·a and the area S = a2·(3)1/2/4 of an equilateral triangle
+ * with the side a and returns the result as two real numbers
+ * (a is an input real-valued parameter). Using this function,
+ * find the perimeters and the areas of three triangles with the
+ * given lengths of the sides.
+ */
+include 'phpHelper.php';
+?>
+<script>
+    function TrianglePS(a){
+        var perimeter = 3 * a ;
+        var area = ((Math.pow(a , 2) / 4 ) * Math.sqrt(5));
+        area = area.toFixed(3);
+        var result  = new Array(perimeter , area);
+
+        return result;
+    }
+    var tr1 = TrianglePS(3);
+    var tr2 = TrianglePS(5);
+    document.getElementById('body').innerHTML = 'perimeter of triangle with side of 3 is ' + tr1[0] + ' area is ' + tr1[1];
+    document.getElementById('body1').innerHTML = 'perimeter of triangle with side of 5 is ' + tr2[0] + ' area is ' + tr2[1];
+
+</script>
