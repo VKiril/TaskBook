d37198ddc9c8d6df1c5bcd0071b9096dd7735897
asus <everest0corp@gmail.com>
2014-11-18 12:28:22 +0200

    the 22'th exercise is done

--- FunctionsJavaScript/22.php
+++ FunctionsJavaScript/22.php
@@ -1,7 +1,16 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: asus
- * Date: 18.11.14
- * Time: 13:05
- */ 
+ * Write a function PowerA3(A) that returns the third degree of a real number
+ * A (A is an input parameter). Using this function, find the third degree of
+ * five given real numbers.
+ */
+include 'phpHelper.php';
+?>
+<script>
+    function PowerA3(A){
+        return Math.pow(A , 3) ;
+    }
+    document.getElementById('body').innerHTML = 'third degree of number 3 is '+PowerA3(3);
+    document.getElementById('body1').innerHTML = 'third degree of number 3 is '+PowerA3(4);
+    document.getElementById('body2').innerHTML = 'third degree of number 3 is '+PowerA3(5);
+</script>
