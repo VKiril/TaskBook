e0337b2a3eb8ccbea3505844bd555163d57a1d6e
asus <everest0corp@gmail.com>
2014-11-18 13:51:43 +0200

    inverse number function ex.29

--- FunctionsJavaScript/28.php
+++ FunctionsJavaScript/28.php
@@ -1,7 +1,23 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: asus
- * Date: 18.11.14
- * Time: 13:06
- */ 
+ * Write a function InvDigits(K) that inverts the order of digits of
+ * a positive integer K and returns the obtained integer (K is an input parameter).
+ * Using this function, invert the order of digits for each of five given integer.
+ */
+include 'phpHelper.php';
+?>
+<script>
+    function InvDigits(K){
+        var x = K.toString().length ;
+        var y = K.toString() ;
+        var inverse = '';
+        for (var i = x ; i >= 0 ; i-- ){
+            inverse += y.charAt(i);
+        }
+        return inverse ;
+    }
+    document.getElementById('body').innerHTML = 'original number 1234567 ' + ' inverted ' +  InvDigits(1234567);
+    document.getElementById('body1').innerHTML ='original number 1698679865 ' + ' inverted ' + InvDigits(1698679865);
+    document.getElementById('body2').innerHTML ='original number 264989548 ' + ' inverted ' + InvDigits(264989548);
+    document.getElementById('body3').innerHTML ='original number 5555555588888888888 ' + ' inverted ' + InvDigits(5555555588888888);
+</script>
