7e81e6c8849a2d3fac9ef5d8e7ba83a96febb298
asus <everest0corp@gmail.com>
2014-11-20 17:26:35 +0200

    doubles an array element which are equal with x

--- ParamPhpJavaScript/10.php
+++ ParamPhpJavaScript/10.php
@@ -1,7 +1,26 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: asus
- * Date: 20.11.14
- * Time: 15:40
- */ 
+ *  Write a procedure DoubleX(A, N, X) that doubles occurrences of all elements equal an
+ * integer X for an array A of N integers. The array A and its size N are input and output
+ * parameters. Using this procedure, double occurrences of elements with given values
+ * XA, XB, XC for three given arrays A, B, C of size NA, NB, NC respectively and output the new
+ * size and elements of each changed array.
+ */
+
+include 'helper.php';
+
+function DoubleX(&$A, $N, $X){
+    for($i = 0; $i < $N ; $i++){
+        if($A[$i] == $X){
+            $A[$i] = $A[$i]*2 ;
+        }
+    }
+}
+
+$arr = [12,32,43,54,3,12,43,2,12,43,12];
+print_r($arr);echo('<br/>');
+DoubleX($arr ,6 , 12);
+print_r($arr);echo('<br/>');
+
+DoubleX($arr ,11 , 12);
+print_r($arr);echo('<br/>');
