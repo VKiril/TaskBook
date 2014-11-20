1c88bb50f4cb27aa9b85191019c88345bafcebbf
asus <everest0corp@gmail.com>
2014-11-20 10:53:20 +0200

    convert from hours in seconds

--- FunctionsJavaScript/51.php
+++ FunctionsJavaScript/51.php
@@ -1,12 +1,20 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: asus
- * Date: 19.11.14
- * Time: 17:13
+ * Write a function IncTime(H, M, S, T) that increases a time interval
+ * in hours H, minutes M, seconds S on T seconds and returns new values
+ * of hours, minutes, and seconds (all numbers are positive integers).
+ * Having input hours H, minutes M, seconds S (as integers) and an
+ * integer T and using the IncTime function, increase the given time
+ * interval on T seconds and output new values of H, M, S.
  */
 include 'phpHelper.php';
 ?>
 <script>
+    function IncTime(H, M, S){
 
+        return (H * 60 * 60 + M * 60 + S);
+    }
+
+    document.getElementById('body').innerHTML = '5 hours 30 minutes and 5 seconds is : ' + IncTime(5,30,5) + ' seconds';
+    document.getElementById('body1').innerHTML = '6 hours 30 minutes and 0 seconds is : ' + IncTime(6,30,0) + ' seconds';
 </script>
