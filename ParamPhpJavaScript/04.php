44bcf444cc18f98250b5f99aecef539519ca87d0
asus <everest0corp@gmail.com>
2014-11-20 15:33:28 +0200

    inverse array

--- ParamPhpJavaScript/04.php
+++ ParamPhpJavaScript/04.php
@@ -1,7 +1,27 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: asus
- * Date: 20.11.14
- * Time: 15:40
- */ 
+ * Write a procedure Inv(A, N) that changes the order of elements of an array
+ * A of N real numbers to inverse one (the array A is an input and output parameter).
+ * Using this procedure, change order of elements of arrays A, B, C
+ * whose sizes are NA, NB, NC respectively.
+ */
+function Inv(&$A, $N){
+    $len = sizeof($A);
+    //echo $len;
+    $inverse = array();
+    $inc =$len-1 ;
+    $i = array();
+    foreach($A as $element){
+        $i[$inc] = $element;
+        $inc --;
+
+    }
+    $A = array();
+    $A = $i;
+
+}
+
+$arr = [2,3,5,1,54,23,21];
+print_r($arr);echo('<br/>');
+Inv($arr , 5);
+print_r($arr);
