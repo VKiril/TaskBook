2a164fdf64c71355022f5c21cf3649db08e2626a
asus <everest0corp@gmail.com>
2014-11-21 16:30:19 +0200

    chage row x with row y from an array

--- ParamPhpJavaScript/23.php
+++ ParamPhpJavaScript/23.php
@@ -1,7 +1,23 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: asus
- * Date: 21.11.14
- * Time: 16:52
- */ 
+ * Write a procedure SwapRow(A, M, N, K1, K2) that exchanges K1-th and K2-th row of an M × N matrix A of real
+ * numbers. The matrix A is an input and output parameter; if K1 or K2 are out of the range 1 to M then the matrix
+ * remains unchanged. Having input an M × N matrix A and two integers K1, K2 and using this procedure, exchange
+ * K1-th and K2-th row of the matrix A.
+ */
+
+function SwapRow(&$A, $K1, $K2){
+    $temp = $A[$K1];
+    $A[$K1] = $A[$K2];
+    $A[$K2] = $temp;
+}
+
+$arr = array(
+    [2,3,45],
+    [4,6,76],
+    [3,2,1 ],
+);
+
+var_dump($arr);
+SwapRow($arr, 0, 1);
+var_dump($arr);
