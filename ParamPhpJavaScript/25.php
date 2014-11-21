d1e27cfd91af55c9b426fc4927496af7e8bd7ca3
asus <everest0corp@gmail.com>
2014-11-21 17:19:55 +0200

    chage cols with rows

--- ParamPhpJavaScript/25.php
+++ ParamPhpJavaScript/25.php
@@ -1,7 +1,30 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: asus
- * Date: 21.11.14
- * Time: 16:52
- */ 
+ * Write a procedure Transp(A, M) that transposes a real-valued square matrix A of order M
+ * (that is, reflects its elements about the main diagonal). The matrix A is an input and output parameter.
+ * Using this procedure, transpose the given matrix A of order M.
+ */
+
+function Transp(&$A, $M){
+    $temp = $A ;
+    /*foreach($temp as $key1 => $val1){
+        foreach($val1 as $key2 => $val2){
+            $A[$key1] = $temp[$key2];
+        }
+    }*/
+    for($i = 0 ; $i<$M ; $i++){
+        for ($j = 0 ; $j < $M ; $j ++){
+            $temp[$i][$j] = $A[$j][$i];
+        }
+    }
+    $A = $temp ;
+}
+
+$arr = array(
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+);
+var_dump($arr);
+Transp($arr , 3);
+var_dump($arr);
