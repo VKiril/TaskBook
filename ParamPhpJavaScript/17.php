c046c02709f2c52f5707c38964ba886f6797e77d
asus <everest0corp@gmail.com>
2014-11-21 15:11:58 +0200

    m x n array and another array :)

--- ParamPhpJavaScript/17.php
+++ ParamPhpJavaScript/17.php
@@ -1,7 +1,41 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: asus
- * Date: 20.11.14
- * Time: 18:28
- */ 
+ * Write a procedure ArrayToMatrCol(A, K, M, N, B) that copies elements of an array A of K
+ * real numbers to an M × N matrix B (by columns). "Superfluous" array elements must be
+ * ignored; if the size of the array is less than the amount of matrix elements then zero
+ * value must be assigned to remaining matrix elements. Two-dimensional array B is an output
+ * parameter. Having input an array A of size K, integers M, N and using this procedure,
+ * create a matrix B and output its elements.
+ */
+include 'helper.php';
+function ArrayToMatrCol(&$A, $K=0, $M  , $N  , $B){
+    $K = $M*$N;
+    $iterator = -1 ;
+    for($i = 0 ; $i < $K ; $i++){
+        $A[$i] = 0 ;
+    }
+
+    for($i = 0 ; $i <$M ; $i++){
+        for ($j  = 0 ;$j < $N ; $j ++){
+            $iterator ++ ;
+
+            $A[$iterator] = $B[$j][$i] ;
+
+            echo($iterator.'<br/>');
+            //print_r($A);
+
+        }//echo($iterator.' gfragr <br/>');
+        //die;
+    }
+}
+
+$arr = array(
+    [1,2,3,4],
+    [2,3,4,5],
+    [2,3,12,3],
+    [8,65,4,9],
+);
+$b = [];
+ArrayToMatrCol($b , 0,4,4,$arr);
+
+print_r($b);
