629cce7701f27ec7d5abfa977a13418a6d34697f
asus <everest0corp@gmail.com>
2014-11-24 16:27:16 +0200

    transform a string to upercase

--- ParamPhpJavaScript/32.php
+++ ParamPhpJavaScript/32.php
@@ -1,7 +1,18 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: asus
- * Date: 21.11.14
- * Time: 16:53
- */ 
+ * Write a procedure UpCaseLat(S) that converts all Latin small letters of a string S to uppercase
+ * (others characters of S must remain unchanged). A string S is an input and output parameter. Using
+ * this procedure, process five given strings.
+ */
+
+include 'helper.php';
+function UpCaseLat(&$S){
+    /*for ($i = 0 ; $i< strlen($S) ; $i++){
+        $S[$i] = strtoupper($S);
+    }*/
+    $S = strtoupper($S);
+}
+
+$var = 'afbg wsognjrtsgolrktm tosbmntdbolsnmfdbt';
+UpCaseLat($var);
+echo($var);
