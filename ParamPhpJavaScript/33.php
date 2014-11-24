410b158cea04b28a6794ce1716a21a5bbf57328b
asus <everest0corp@gmail.com>
2014-11-24 16:29:56 +0200

    transform a string to lowercase

--- ParamPhpJavaScript/33.php
+++ ParamPhpJavaScript/33.php
@@ -1,7 +1,16 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: asus
- * Date: 21.11.14
- * Time: 16:53
- */ 
+ * Write a procedure LowCaseLat(S) that converts all Latin capital letters of a string S to lowercase
+ * (others characters of S must remain unchanged). A string S is an input and output parameter. Using this
+ * procedure, process five given strings.
+ */
+include 'helper.php';
+
+function LowCaseLat(&$S) {
+    $S = strtolower ($S);
+}
+
+
+$var = 'ALDSNGBR JGNSDK JNGSKJGNH SKJGNWOEINHR D8KLJGNBTKLJXGN PSIJRHGTFD';
+LowCaseLat($var);
+echo($var);
