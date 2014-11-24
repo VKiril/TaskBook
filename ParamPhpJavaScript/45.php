99bf6776158ca58f6d67342561f7e913fd3e89f6
asus <everest0corp@gmail.com>
2014-11-24 19:37:37 +0200

    dec to hex

--- ParamPhpJavaScript/45.php
+++ ParamPhpJavaScript/45.php
@@ -1,7 +1,16 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: asus
- * Date: 24.11.14
- * Time: 19:24
- */ 
+ * Write a string function DecToHex(N) that returns a string containing the hexadecimal representation of a
+ * nonnegative integer N. The string consists of characters "0"–"9", "A"–"F" and does not contain leading zeros
+ * (except for the representation of zero number). Using this function, output hexadecimal representations of five
+ * given integers.
+ */
+include'helper.php';
+function DecToHex($N){
+    echo dechex ($N).'<br/>';
+}
+
+DecToHex(65535);
+DecToHex(255);
+DecToHex(127);
+
