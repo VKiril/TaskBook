e4dc31a7e66f765265957e0f707df5370ba3df23
asus <everest0corp@gmail.com>
2014-11-24 16:39:02 +0200

    remove character

--- ParamPhpJavaScript/34.php
+++ ParamPhpJavaScript/34.php
@@ -1,7 +1,21 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: asus
- * Date: 21.11.14
- * Time: 16:53
- */ 
+ * Write a procedure TrimLeftC(S, C) that removes all leading characters equal a character C from a string S.
+ * A string S is an input and output parameter. Having input a character C and five strings and using this procedure,
+ * process the given strings.
+ */
+
+function TrimLeftC(&$S, $C){
+    for($i = 0 ; $i < strlen($S) ; $i++){
+        if($S[$i] == $C){
+            $S[$i] = '';
+        }
+    }
+
+
+    //$S = str_replace($S , $C ,'');
+}
+
+$var = ' CCCCCCCCCCCCCCCCCCCLLLLLLLLLLLLLLLLLLLLLLLLLLLL';
+TrimLeftC($var , 'C');
+echo($var);
