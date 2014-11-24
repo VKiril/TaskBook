40ff5e6e011e53992ae3a9a399ee4e8d93eb4e9b
asus <everest0corp@gmail.com>
2014-11-24 16:44:01 +0200

    remove character

--- ParamPhpJavaScript/35.php
+++ ParamPhpJavaScript/35.php
@@ -1,7 +1,21 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: asus
- * Date: 21.11.14
- * Time: 16:53
- */ 
+ * Write a procedure TrimRightC(S, C) that removes all trailing characters equal a character C from a string S.
+ * A string S is an input and output parameter. Having input a character C and five strings and using this procedure,
+ * process the given strings.
+ */
+
+function TrimRightC(&$S, $C){
+    for($i = 0 ; $i < strlen($S) ; $i++){
+        if($S[$i] == $C){
+            $S[$i] = '';
+        }
+    }
+
+
+    //$S = str_replace($S , $C ,'');
+}
+
+$var = ' CCCCCCCCCCCCCCCCCCCLLLLLLLLLLLLLLLLLLLLLLLLLLLL';
+TrimRightC($var , 'C');
+echo($var);
