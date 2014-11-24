97b7390964dbed25ec23c0ce958426c695c87676
asus <everest0corp@gmail.com>
2014-11-24 15:43:45 +0200

    the 29'th exercise is done

--- ParamPhpJavaScript/29.php
+++ ParamPhpJavaScript/29.php
@@ -1,7 +1,31 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: asus
- * Date: 21.11.14
- * Time: 16:52
- */ 
+ * Write a procedure SortCols(A, M, N) that rearrange columns of an M × N matrix A of integers in ascending
+ * lexicographic order (that is, for comparison of two columns their first distinct elements with the equal
+ * order numbers must be compared). Two-dimensional array A is an input and output parameter. Using this procedure,
+ * sort columns of a given M × N matrix A.
+ */
+include'helper.php';
+function SortCols(&$a, $m, $n){
+
+    for($i = 0 ; $i < $m ; $i++){
+        for($j = 0 ; $j < $m ; $j++){
+            if($a[0][$i] > $a[0][$j] ){
+                for($k = 1 ; $k < $n ; $k++){
+                    $temp = $a[$i][$k];
+                    $a[$i][$k] = $a[$j][$k];
+                    $a[$j][$k] = $temp ;
+                }
+            }
+        }
+    }
+}
+
+$arr = array(
+    [3,2,1],
+    [4,5,6],
+    [7,8,9],
+);
+var_dump($arr);
+SortCols($arr , 3,3);
+var_dump($arr);
