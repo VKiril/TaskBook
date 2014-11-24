6eaa36cb4e9e2a1f29832003df59bf46afe6e3f1
asus <everest0corp@gmail.com>
2014-11-24 16:22:24 +0200

    concatenate string

--- ParamPhpJavaScript/31.php
+++ ParamPhpJavaScript/31.php
@@ -1,8 +1,17 @@
-
 <?php
 /**
- * Created by PhpStorm.
- * User: asus
- * Date: 21.11.14
- * Time: 16:53
- */ 
+ * Write a string function FillStr(S, N) that returns a string of length N containing repeating
+ * copies of the template string S (the last copy of S may be contained partially in the resulting string).
+ * Having input an integer N and five template strings and using this function, create five resulting strings
+ * of length N.
+ */
+include 'helper.php';
+function FillStr($S, $N){
+    $var = '' ;
+    for ($i = 0 ; $i < $N ; $i++ ){
+        $var  = $var.$S;
+    }
+    return $var ;
+}
+
+echo(FillStr('Hello ' , 6));
