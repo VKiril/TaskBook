9ca276f4b839b521dacd6ebad185d5004b88e57a
asus <everest0corp@gmail.com>
2014-11-24 17:59:32 +0200

    return lkast position of a word in a string

--- ParamPhpJavaScript/38.php
+++ ParamPhpJavaScript/38.php
@@ -1,7 +1,22 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: asus
- * Date: 21.11.14
- * Time: 16:53
- */ 
+ * Write an integer function PosLast(S0, S) that searches for the last occurrence of a string S0 within a
+ * string S and returns the order number of the first character of this occurrence. If the string S does not
+ * contain occurrences of S0 then the function returns 0. Output return values of this function for five given
+ * pairs of strings (S0, S).
+ */
+
+include'helper.php';
+function PosLast($S0, $S, $K=0, $N=0){
+    if(strpos($S0 , $S)) {
+        echo strpos($S0 , $S).'<br/>';
+    }
+    if(strrpos($S0 , $S)) {
+        echo strrpos($S0 , $S);
+        $result = strrpos($S0 , $S);
+    }
+    return $result;
+
+}
+
+PosLast('a a a a, a fost odata fost ...' , 'fost');
