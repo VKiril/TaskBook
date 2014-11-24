ebc87ccbaf1163657bb289bffd7e6293df84a43e
asus <everest0corp@gmail.com>
2014-11-24 19:43:01 +0200

    hex to dec

--- ParamPhpJavaScript/47.php
+++ ParamPhpJavaScript/47.php
@@ -1,7 +1,16 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: asus
- * Date: 24.11.14
- * Time: 19:24
- */ 
+ * Write an integer function HexToDec(S) that returns a nonnegative integer whose hexadecimal representation
+ * is contained in a string parameter S. The parameter S consists of characters "0"–"9", "A"–"F" and does not
+ * contain leading zeros (except for the representation of zero number). Using this function, output five integers
+ * whose hexadecimal representations are given.
+ */
+include'helper.php';
+function HexToDec($N){
+    echo hexdec  ($N).'<br/>';
+}
+
+HexToDec('ffff');
+HexToDec('ff');
+HexToDec('f');
+HexToDec('acbd');
