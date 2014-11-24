3a79d2a3e539cd772bfd60456d7c19b2b0caeed2
asus <everest0corp@gmail.com>
2014-11-24 19:39:45 +0200

    BIN TO DEC

--- ParamPhpJavaScript/46.php
+++ ParamPhpJavaScript/46.php
@@ -1,7 +1,17 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: asus
- * Date: 24.11.14
- * Time: 19:24
- */ 
+ * Write an integer function BinToDec(S) that returns a nonnegative integer whose binary representation is
+ * contained in a string parameter S. The parameter S consists of characters "0", "1" and does not contain
+ * leading zeros (except for the representation of zero number). Using this function, output five integers
+ * whose binary representations are given.
+ */
+
+function BinToDec($N){
+    echo bindec($N).'<br/>';
+}
+
+include'helper.php';
+
+BinToDec(1111111111111111);
+BinToDec(11111111);
+BinToDec(1111);
