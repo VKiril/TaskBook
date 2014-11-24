b74ca81cfc0d50b806f849eaa292f6acac597e5a
asus <everest0corp@gmail.com>
2014-11-24 16:15:47 +0200

    validate a string

--- ParamPhpJavaScript/30.php
+++ ParamPhpJavaScript/30.php
@@ -1,7 +1,32 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: asus
- * Date: 21.11.14
- * Time: 16:52
- */ 
+ * Write an integer function IsIdent(S) that indicates whether a string S is a valid identifier, that is,
+ * a nonempty string that does not begin with a digit and contains Latin letters, digits, and a character "_" only.
+ * If S is a valid identifier then the function returns 0. If S is an empty string or begins with a digit then the
+ * function returns −1 or −2 respectively. If S contains invalid characters then the function returns the order
+ * number of the first invalid character. Using this function, check five given strings.
+ */
+
+function IsIdent($S){
+
+    if($S =="" ){
+        return -1;
+    } elseif( ctype_digit($S) || $S[0] == '_' ){
+        return -2;
+    } else {
+        for($i = 0 ; $i< strlen($S) ; $i++){
+            if(ctype_digit($S[$i]) || ctype_alpha($S[$i]) ||$S[$i] == '_'  ){
+                return 0;
+            }
+    }
+    }
+
+}
+
+
+
+echo '<br/>'.IsIdent('1asf').'<br/>';
+echo '<br/>'.IsIdent('fsbdtkbnfstbt').'<br/>';
+echo '<br/>'.IsIdent('_vsfdbtsdfbt').'<br/>';
+echo '<br/>'.IsIdent('Ajsbgtsiudfv').'<br/>';
+echo '<br/>'.IsIdent('').'<br/>';
