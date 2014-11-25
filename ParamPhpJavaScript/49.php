c9eb796a287d8109c645e3598aad3e526d879870
asus <everest0corp@gmail.com>
2014-11-25 13:08:12 +0200

    file funtion ,count number of rows

--- ParamPhpJavaScript/49.php
+++ ParamPhpJavaScript/49.php
@@ -1,7 +1,27 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: asus
- * Date: 24.11.14
- * Time: 19:24
- */ 
+ * Write an integer function LineCount(S) that returns the amount of lines in a text file called S.
+ * If the required file does not exist then the function returns −1. Using this function, output the
+ * amount of lines for three text files with given names.
+ */
+include 'helper.php';
+echo("<div style='width: 800px; margin-left: 20% ;' >");
+function LineCount($S){
+    $file= $S;
+    $lineCount = 0;
+    $handle = fopen($file, "r");
+    while(!feof($handle)){
+        $line = fgets($handle);
+        $lineCount++;
+    }
+
+    fclose($handle);
+
+    echo "<br/> number of lines is : ".$lineCount;
+}
+
+echo readfile('test.txt').'<br/>';
+
+LineCount("test.txt");
+
+echo('</div>');
