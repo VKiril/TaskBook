f78ab253e336e2c8a8c645faf1482eea2c0cce54
asus <everest0corp@gmail.com>
2014-11-25 12:53:32 +0200

    file funtion

--- ParamPhpJavaScript/48.php
+++ ParamPhpJavaScript/48.php
@@ -1,7 +1,23 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: asus
- * Date: 24.11.14
- * Time: 19:24
- */ 
+ * Write an integer function IntFileSize(S) that returns the amount of components in a binary file of integers
+ * called S. If the required file does not exist then the function returns −1. Using this function, output the
+ * amount of components for three binary files of integers with given names.
+ */
+
+
+include 'helper.php';
+
+function IntFileSize($S){
+    $file = fopen( $S, 'r');
+    $var = fread($file,filesize($S));
+    $arr = explode(' ', $var);
+    echo('<br/>'.sizeof($arr));
+    fclose($file);
+}
+
+
+IntFileSize('test.txt');
+
+
+
