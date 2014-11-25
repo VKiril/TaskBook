94a5c29278f5079f514e630d886dfe9e661e98c1
asus <everest0corp@gmail.com>
2014-11-25 13:25:56 +0200

    inverse file text word position

--- ParamPhpJavaScript/50.php
+++ ParamPhpJavaScript/50.php
@@ -4,7 +4,27 @@
  * to inverse one. If the required file does not exist then the procedure performs no actions. Using this procedure,
  * process three binary files of integers with given names.
  */
-
+include 'helper.php';
 function InvIntFile(&$S){
+    if(!file_exists($S)){
+        return 'no action';
+    }
+    $file = fopen( $S, 'r+');
+    $var = fread($file,filesize($S));
+    $arr = explode(' ', $var);
+    $inverse = '';
+    foreach($arr as $element){
+        $inverse = $element.' '.$inverse;
+    }
+
+    file_put_contents($S , $inverse);
+    //echo $inverse;
+    fclose($file);
 
 }
+
+$var = 'test.txt';
+echo readfile($var).'<br/><br/><br/>';
+
+InvIntFile($var);
+echo readfile($var).'<br/>';
