dacf864737e7918df110613a0d3e6e70a90843af
asus <everest0corp@gmail.com>
2014-11-26 16:59:16 +0200

    copy rows in different files

--- ParamPhpJavaScript/53.php
+++ ParamPhpJavaScript/53.php
@@ -1,9 +1,9 @@
 <?php
+
 /**
- * Write a procedure SplitIntFile(S0, K, S1, S2) that copies first K (≥ 0) components of an existing file
- * called S0 to a new file called S1 and other components of this file to a new file called S2. All files are
- * assumed to be binary files of integers; one of the resulting files may be empty. Apply this procedure to a given
- * file called S0 using given values of K, S1, S2.
+ * Write a procedure SplitText(S0, K, S1, S2) that copies first K (≥ 0) lines of an existing text file called S0
+ * to a new text file called S1 and other lines of this file to a new text file called S2 (one of the resulting
+ * files may be empty). Apply this procedure to a given file called S0 using given values of K, S1 S2.
  */
 include'helper.php';
 function SplitIntFile($S0, $K, $S1, $S2){
--- ParamPhpJavaScript/54.php
+++ ParamPhpJavaScript/54.php
@@ -1,7 +1,8 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: asus
- * Date: 25.11.14
- * Time: 14:26
- */ 
+ * Write a procedure SplitIntFile(S0, K, S1, S2) that copies first K (≥ 0) components of an existing file
+ * called S0 to a new file called S1 and other components of this file to a new file called S2. All files are
+ * assumed to be binary files of integers; one of the resulting files may be empty. Apply this procedure to a given
+ * file called S0 using given values of K, S1, S2.
+ */
+
