7bee1374e30c5fb4b68d01e7e9f5a874d839dceb
asus <everest0corp@gmail.com>
2014-11-26 16:29:10 +0200

    delete row number from file content

--- ParamPhpJavaScript/52.php
+++ ParamPhpJavaScript/52.php
@@ -5,7 +5,39 @@
  * in Param51). If file lines do not contain order numbers then the procedure performs no actions. Apply this
  * procedure to a given text file.
  */
+include'helper.php';
+function RemoveLineNumbers($S = 'test.txt' ){
+    $handle = fopen($S, "r");
+    $temp = fopen('file.txt', "w");
+    while(! feof($handle))
+    {
+        $a = fgets($handle);
+        $pieces = explode(" ", $a);
+        //var_dump($pieces);
+        for ($i = 0 ; $i< sizeof($pieces) ; $i++){
+            if(!ctype_alpha ($pieces[$i])){
+                unset($pieces[$i]);
 
-function RemoveLineNumbers($S){
+                /*if($pieces[$i+1]==' '){
+                    for($j = $i ; $j< sizeof($pieces);$j++){
+                        if($pieces[$j]== ' '){
 
+                        }
+                    }
+                }*/
+            }
+        }
+       // echo $a.'<br/>';
+        //var_dump($pieces);
+        $b = implode(' ' , $pieces);
+        echo $b."<br/>";
+        fwrite($temp , $b);
+
+
+    }
+
+    fclose($temp);
+    fclose($handle);
 }
+
+RemoveLineNumbers();
