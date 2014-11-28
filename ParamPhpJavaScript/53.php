b72fa4edeba564935f72de90716ebff034929df8
asus <everest0corp@gmail.com>
2014-11-28 12:30:05 +0200

    copy words from one file to another two

--- ParamPhpJavaScript/53.php
+++ ParamPhpJavaScript/53.php
@@ -5,40 +5,50 @@
  * to a new text file called S1 and other lines of this file to a new text file called S2 (one of the resulting
  * files may be empty). Apply this procedure to a given file called S0 using given values of K, S1 S2.
  */
+
 include'helper.php';
-function SplitIntFile($S0, $K, $S1, $S2){
+function SplitText($S0, $K, $S1, $S2){
     $n = 0 ;
 
     $handle = fopen($S0, "r");
     $handleS1 = fopen($S1 , 'w');
     $handleS2 = fopen($S2 , 'w');
-    while(! feof($handle))
-    {
-        $n++ ;
-        $line = fgets($handle);
-    }
-    fclose($handle);
-    if($K > $n){
-        echo'k is greater than file row numbers';
-        return 1;
-    }
-    $n = 0 ;
-    $handle = fopen($S0, "r");
-    while(! feof($handle))
-    {
-        $n++ ;
-        $line = fgets($handle);
-        if($n < $K){
-            fwrite($handleS1 , $line);
-        } else {
-            fwrite($handleS2 , $line);
-        }
 
+    $var  = fread($handle,filesize($S0));
+    $arr = explode(' ' , $var);
+    //var_dump($arr);
+
+    $arrS1 = [];
+    for($i = 0 ; $i < $K ; $i ++){
+        $arrS1[$i] = $arr[$i];
     }
+    //var_dump($arrS1);
+    $arrS2 = [];
+    for($i = $K ; $i < sizeof($arr) ; $i ++){
+        $arrS2[$i] = $arr[$i];
+    }
+
+    $texts1 = implode(' ',$arrS1);
+    $texts2 = implode(' ',$arrS2);
+    echo $texts1;
+    //var_dump($arrS2);
+    fwrite($handleS1 , $texts1);
+    fwrite($handleS2 , $texts2);
+
+
+
+    //die;
+
+
+
+    //echo $var;
+    //die;
+
+
 
     fclose($handle);
     fclose($handleS1);
     fclose($handleS2);
 }
 
-SplitIntFile('test.txt' , 6 , 's0.txt' , 's1.txt');
+SplitText('test.txt' , 60 , 's0.txt' , 's1.txt');
--- ParamPhpJavaScript/54.php
+++ ParamPhpJavaScript/54.php
@@ -6,3 +6,40 @@
  * file called S0 using given values of K, S1, S2.
  */
 
+include'helper.php';
+function SplitIntFile($S0, $K, $S1, $S2){
+    $n = 0 ;
+
+    $handle = fopen($S0, "r");
+    $handleS1 = fopen($S1 , 'w');
+    $handleS2 = fopen($S2 , 'w');
+    while(! feof($handle))
+    {
+        $n++ ;
+        $line = fgets($handle);
+    }
+    fclose($handle);
+    if($K > $n){
+        echo'k is greater than file row numbers';
+        return 1;
+    }
+    $n = 0 ;
+    $handle = fopen($S0, "r");
+    while(! feof($handle))
+    {
+        $n++ ;
+        $line = fgets($handle);
+        if($n < $K){
+            fwrite($handleS1 , $line);
+        } else {
+            fwrite($handleS2 , $line);
+        }
+
+    }
+
+    fclose($handle);
+    fclose($handleS1);
+    fclose($handleS2);
+}
+
+SplitIntFile('test.txt' , 6 , 's0.txt' , 's1.txt');
