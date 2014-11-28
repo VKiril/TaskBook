7af20ae273affee99b93bd6339aeb55264e4c740
asus <everest0corp@gmail.com>
2014-11-28 14:31:25 +0200

    from bin to text

--- ParamPhpJavaScript/56.php
+++ ParamPhpJavaScript/56.php
@@ -1,7 +1,6 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: asus
- * Date: 25.11.14
- * Time: 14:27
+ * Write a procedure TextToStringFile(S) that converts a text file called S to a new binary file of strings
+ * with the same name. Using this procedure, convert two given text files with the names S1, S2 to binary files
+ * of strings.
  */ 
--- ParamPhpJavaScript/57.php
+++ ParamPhpJavaScript/57.php
@@ -1,7 +1,9 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: asus
- * Date: 25.11.14
- * Time: 14:27
- */ 
+ * Write a procedure EncodeText(S, K) that encrypts the contents of a text file called S using the right cyclic
+ * shift of any Latin letter by K positions of the English alphabet (0 < K < 10). For instance, if K = 3 then
+ * the letter "A" is encoded by the letter "D", "a" is encoded by "d", "B" is encoded by "E", "z" is encoded
+ * by "c", and so on. Blank characters and punctuation marks should not be changed. Having input an integer K
+ * and using this procedure, encrypt a text file with the given name.
+ */
+
