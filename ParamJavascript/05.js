f135c047c05dbe43163d0e96d89ee2dbba892bc8
asus <everest0corp@gmail.com>
2014-12-03 18:35:43 +0200

    smooth

--- ParamJavascript/05.js
+++ ParamJavascript/05.js
@@ -1,3 +1,28 @@
 /**
- * Created by asus on 03.12.14.
+ * Write a procedure Smooth1(A, N) that performs smoothing an array A of N real numbers
+ * as follows: each element AK is replaced with the average of initial values of K first
+ * elements of the given array A. The array A is an input and output parameter. Using five
+ * calls of this procedure, perform smoothing a given array A of N real numbers five times
+ * successively; output array elements after each smoothing.
  */
+function Smooth1($A, $N){
+    if($N> 2){
+        $k = $N - 2 ;
+    }else{
+        $k = $N;
+    }
+
+    $average = 0 ;
+    for ($i = 0 ; $i > $k ; $i ++){
+        $average += $A[$i];
+    }
+    $average = $average / $k ;
+
+    $A[$k]=$average;
+
+    return $A;
+}
+
+$arr = [12,56,32,48,20,65,78,11,22,10,1];
+
+console.log(Smooth1($arr,10));
