03bb835abdd8773653ae5674c0bd787152fd1d25
asus <everest0corp@gmail.com>
2014-12-03 18:59:12 +0200

    remove for inc

--- ParamJavascript/09.js
+++ ParamJavascript/09.js
@@ -1,3 +1,26 @@
 /**
  * Created by asus on 03.12.14.
  */
+
+function RemoveForInc($A, $N  ){
+    $N = $A.length;
+    //cho($N.'<br/>');
+    $temp = $A[0] ;
+    for($i = 1 ; $i< $N ; $i++){
+        if($A[$i]  < $temp){
+            // echo($A[$i]."<br/>");
+            delete ($A[$i]);
+        } else {
+            $temp = $A[$i];
+        }
+    }
+    $N =$A.length ;
+    var result =  [$A , $N];
+    return result;
+
+}
+
+$arr = [12,23,24,54,21,3,4,12,124,456,457,345,457];
+var a = RemoveForInc($arr , 10) ;
+console.log(a[0] );
+console.log(a[1] );
