a1aa8ab5b12c9de70057765618156fc966d131cb
asus <everest0corp@gmail.com>
2014-12-03 18:50:54 +0200

    REMOVE

--- ParamJavascript/08.js
+++ ParamJavascript/08.js
@@ -1,3 +1,20 @@
 /**
- * Created by asus on 03.12.14.
+ *Write a procedure RemoveX(A, N, X) that removes all elements equal an integer X
+ * from an array A of N integers. The array A and its size N are input and output parameters.
+ * Using this procedure, remove elements with given values XA, XB, XC from three given arrays
+ * A, B, C of size NA, NB, NC respectively and output the new size and elements of each changed array.
  */
+function  RemoveX($A, $N, $X){
+    $N = $A.length;
+    for($i = 0 ; $i < $N; $i++){
+        if($A[$i] == $X){
+            delete ($A[$i]);
+        }
+    }
+
+    return $A;
+}
+
+$arr = [1,32,54,21,65,78,4,3,2,25,6,7,8,4,5,6,7,8,9];
+
+console.log(RemoveX($arr , 10 , 1));
