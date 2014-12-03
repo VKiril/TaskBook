007aab0052f4746e101ac062d1196a5a5b3487c5
asus <everest0corp@gmail.com>
2014-12-03 18:31:41 +0200

    inverse

--- ParamJavascript/04.js
+++ ParamJavascript/04.js
@@ -1,3 +1,22 @@
 /**
  * Created by asus on 03.12.14.
  */
+function Inv($A, $N){
+    $len = $A.length;
+    //echo $len;
+    $inverse = [];
+    $inc =$len-1 ;
+    $i = [];
+    for($element in $A){
+        $i[$inc] = $element;
+        $inc --;
+
+    }
+    $A = [];
+    $A = $i;
+    return $A;
+}
+
+$arr = [2,3,5,1,54,23,21];
+
+console.log(Inv($arr , 1));
