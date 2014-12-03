8a56946b44bc5b5550b0f4c4f101b1860567f0da
asus <everest0corp@gmail.com>
2014-12-03 19:02:23 +0200

    double x

--- ParamJavascript/10.js
+++ ParamJavascript/10.js
@@ -1,3 +1,20 @@
 /**
- * Created by asus on 03.12.14.
+ *  Write a procedure DoubleX(A, N, X) that doubles occurrences of all elements equal an
+ * integer X for an array A of N integers. The array A and its size N are input and output
+ * parameters. Using this procedure, double occurrences of elements with given values
+ * XA, XB, XC for three given arrays A, B, C of size NA, NB, NC respectively and output the new
+ * size and elements of each changed array.
  */
+
+function DoubleX($A, $N, $X){
+    for($i = 0; $i < $N ; $i++){
+        if($A[$i] == $X){
+            $A[$i] = $A[$i]*2 ;
+        }
+    }
+    return $A;
+}
+
+$arr = [12,32,43,54,3,12,43,2,12,43,12];
+
+console.log(DoubleX($arr , 11 , 9));
