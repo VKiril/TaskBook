79105f2deed562558f56bee07e046d21e6c129a7
asus <everest0corp@gmail.com>
2014-12-04 12:39:33 +0200

    transposes

--- ParamJavascript/25.js
+++ ParamJavascript/25.js
@@ -1,3 +1,19 @@
 /**
- * Created by asus on 03.12.14.
+ * Write a procedure Transp(A, M) that transposes a real-valued square matrix A of order M
+ * (that is, reflects its elements about the main diagonal). The matrix A is an input and output parameter.
+ * Using this procedure, transpose the given matrix A of order M.
  */
+
+function Transp($A, $M){
+    var $temp = $A ;
+    for(var $i = 0 ; $i<$M ; $i++){
+        for (var $j = 0 ; $j < $M ; $j ++){
+            $temp[$i][$j] = $A[$j][$i];
+        }
+    }
+    return $temp ;
+}
+
+
+$arr = [[2,3,45],[4,6,76],[3,2,1 ]];
+console.log(Transp($arr , 3));
