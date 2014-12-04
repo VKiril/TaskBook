91523da9801c634058f2f27b9bac64225c76f10e
asus <everest0corp@gmail.com>
2014-12-04 13:14:06 +0200

    remove character

--- ParamJavascript/34.js
+++ ParamJavascript/34.js
@@ -1,3 +1,20 @@
 /**
- * Created by asus on 03.12.14.
+ * Write a procedure TrimLeftC(S, C) that removes all leading characters equal a character C from a string S.
+ * A string S is an input and output parameter. Having input a character C and five strings and using this procedure,
+ * process the given strings.
  */
+
+function TrimLeftC($S, $C){
+
+
+
+    for($i = 0 ; $i < $S.length ; $i++){
+        $S = $S.replace($C,'');
+    }
+    return $S;
+
+}
+
+
+var $var = ' CCCCCCCCCCCCCCCCCCCLLLLLLLLLLLLLLLLLLLLLLLLLLLL';
+console.log(TrimLeftC($var , 'C'));
