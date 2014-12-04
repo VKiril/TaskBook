a7ffc3d504bcb69437207b8cb0edf4cfd661cf6e
asus <everest0corp@gmail.com>
2014-12-04 13:08:44 +0200

    to lower case

--- ParamJavascript/33.js
+++ ParamJavascript/33.js
@@ -1,3 +1,14 @@
 /**
- * Created by asus on 03.12.14.
+ * Write a procedure LowCaseLat(S) that converts all Latin capital letters of a string S to lowercase
+ * (others characters of S must remain unchanged). A string S is an input and output parameter. Using this
+ * procedure, process five given strings.
  */
+
+
+function LowCaseLat($S) {
+    $S = $S.toLowerCase();
+    return $S;
+}
+
+var $var = 'ALDSNGBR JGNSDK JNGSKJGNH SKJGNWOEINHR D8KLJGNBTKLJXGN PSIJRHGTFD';
+console.log(LowCaseLat($var));
