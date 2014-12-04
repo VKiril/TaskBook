fcd85ea1408b09a0beefd0922b1a160f60adbbe9
asus <everest0corp@gmail.com>
2014-12-04 13:36:53 +0200

    trim right

--- ParamJavascript/36.js
+++ ParamJavascript/36.js
@@ -1,3 +1,19 @@
 /**
- * Created by asus on 03.12.14.
+ * Write a procedure TrimRightC(S, C) that removes all trailing characters equal a character C from a string S.
+ * A string S is an input and output parameter. Having input a character C and five strings and using this procedure,
+ * process the given strings.
  */
+
+function TrimRightC($S, $C){
+
+
+for($i = 0 ; $i < $S.length ; $i++){
+    $S = $S.replace($C,'');
+}
+return $S;
+
+}
+
+
+var $var = ' CCCCCCCCCCCCCCCCCCCLLLLLLLLLLLLLLLLLLLLLLLLLLLL';
+console.log(TrimRightC($var , 'C'));
