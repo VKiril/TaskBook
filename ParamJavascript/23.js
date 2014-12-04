c641f6e2d6c5596394a1f07e286d02a648a2a9b7
asus <everest0corp@gmail.com>
2014-12-04 12:31:23 +0200

    swap row

--- ParamJavascript/23.js
+++ ParamJavascript/23.js
@@ -1,3 +1,14 @@
 /**
- * Created by asus on 03.12.14.
+ * Write a procedure SwapRow(A, M, N, K1, K2) that exchanges K1-th and K2-th row of an M × N matrix A of real
+ * numbers. The matrix A is an input and output parameter; if K1 or K2 are out of the range 1 to M then the matrix
+ * remains unchanged. Having input an M × N matrix A and two integers K1, K2 and using this procedure, exchange
+ * K1-th and K2-th row of the matrix A.
  */
+function SwapRow($A, $K1, $K2){
+    var  $temp = $A[$K1];
+    $A[$K1] = $A[$K2];
+    $A[$K2] = $temp;
+    return $A;
+}
+$arr = [[2,3,45],[4,6,76],[3,2,1 ]];
+console.log(SwapRow($arr, 0, 1));
