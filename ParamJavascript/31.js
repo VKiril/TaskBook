78b6334f1838054952e433e8243a15a676dd9eb6
asus <everest0corp@gmail.com>
2014-12-04 13:03:31 +0200

    fill string

--- ParamJavascript/31.js
+++ ParamJavascript/31.js
@@ -1,3 +1,14 @@
 /**
- * Created by asus on 03.12.14.
+ * Write a string function FillStr(S, N) that returns a string of length N containing repeating
+ * copies of the template string S (the last copy of S may be contained partially in the resulting string).
+ * Having input an integer N and five template strings and using this function, create five resulting strings
+ * of length N.
  */
+function FillStr($S, $N){
+    var $var = '' ;
+    for (var $i = 0 ; $i < $N ; $i++ ){
+        $var  = $var+$S;
+    }
+    return $var ;
+}
+console.log((FillStr('Hello ' , 6)));
