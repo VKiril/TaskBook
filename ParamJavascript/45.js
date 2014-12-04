cd19391082776e42eb563ed94170f44e323b2811
asus <everest0corp@gmail.com>
2014-12-04 14:38:27 +0200

    bin to dec

--- ParamJavascript/45.js
+++ ParamJavascript/45.js
@@ -1,3 +1,12 @@
 /**
  * Created by asus on 03.12.14.
  */
+function BinToDec($N){
+    var digit = parseInt($N, 2);
+    return digit;
+}
+
+
+console.log(BinToDec(1111111111111111));
+console.log(BinToDec(11111111));
+console.log(BinToDec(1111));
