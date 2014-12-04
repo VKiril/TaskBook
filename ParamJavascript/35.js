2900a0599d4d8c5935937aea668c3998cc082d10
asus <everest0corp@gmail.com>
2014-12-04 13:33:03 +0200

    inverse string

--- ParamJavascript/35.js
+++ ParamJavascript/35.js
@@ -1,3 +1,15 @@
 /**
  * Created by asus on 03.12.14.
  */
+function InvStr($S, $K, $N ){
+    if($K > $S.length){
+        return '';
+    }
+    var $var ='';
+    for(var $i = $S.length-1 ; $i>$K ;$i--){
+        $var = $var+$S[$i];
+    }
+
+    return $var;
+}
+console.log(InvStr('gnrewuri gnsrk gjnrskgjn sejgn sdtkjgnfs ikgnsre' , 15 ));
