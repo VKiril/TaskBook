2b0ba704699adeffa125ec2971dda7361bfb2022
asus <everest0corp@gmail.com>
2014-12-04 13:06:18 +0200

    upper case

--- ParamJavascript/32.js
+++ ParamJavascript/32.js
@@ -1,3 +1,16 @@
 /**
- * Created by asus on 03.12.14.
+ * Write a procedure UpCaseLat(S) that converts all Latin small letters of a string S to uppercase
+ * (others characters of S must remain unchanged). A string S is an input and output parameter. Using
+ * this procedure, process five given strings.
  */
+
+function UpCaseLat($S){
+    /*for ($i = 0 ; $i< strlen($S) ; $i++){
+     $S[$i] = strtoupper($S);
+     }*/
+   var a = $S.toUpperCase();
+    return a;
+}
+
+var $var = 'afbg wsognjrtsgolrktm tosbmntdbolsnmfdbt';
+console.log(UpCaseLat($var));
