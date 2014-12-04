90a2f244bfb2dcaf7a4085774a9258a4873e77f9
asus <everest0corp@gmail.com>
2014-12-04 14:43:23 +0200

    hex to dec

--- ParamJavascript/47.js
+++ ParamJavascript/47.js
@@ -1,3 +1,17 @@
 /**
- * Created by asus on 03.12.14.
+ * Write an integer function HexToDec(S) that returns a nonnegative integer whose hexadecimal representation
+ * is contained in a string parameter S. The parameter S consists of characters "0"–"9", "A"–"F" and does not
+ * contain leading zeros (except for the representation of zero number). Using this function, output five integers
+ * whose hexadecimal representations are given.
  */
+
+function HexToDec($N){
+    $N = ($N + '')
+        .replace(/[^a-f0-9]/gi, '');
+    return parseInt($N, 16);
+}
+
+console.log(HexToDec('ffff'));
+console.log(HexToDec('ff'));
+console.log(HexToDec('f'));
+console.log(HexToDec('acbd'));
