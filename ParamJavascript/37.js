1f1bbb6fb644c7a3d140df8c2784da899d75dd8f
asus <everest0corp@gmail.com>
2014-12-04 13:42:17 +0200

    strpos

--- ParamJavascript/37.js
+++ ParamJavascript/37.js
@@ -1,3 +1,12 @@
 /**
  * Created by asus on 03.12.14.
  */
+
+function PosSub($S0, $S){
+    if($S0.indexOf($S)) {
+        return $S0.indexOf($S);
+    }
+}
+
+
+console.log(PosSub('a a a a, a fost odata fost ...' , 'fost'));
