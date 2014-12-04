8e1dc5e3501aed68b071af8bc75fd14745e007c0
asus <everest0corp@gmail.com>
2014-12-04 12:37:04 +0200

    swap col

--- ParamJavascript/24.js
+++ ParamJavascript/24.js
@@ -1,3 +1,18 @@
 /**
- * Created by asus on 03.12.14.
+ * Write a procedure SwapCol(A, M, N, K1, K2) that exchanges K1-th and K2-th column of an M × N
+ * matrix A of real numbers. The matrix A is an input and output parameter; if K1 or K2 are out
+ * of the range 1 to N then the matrix remains unchanged. Having input an M × N matrix A and two
+ * integers K1, K2 and using this procedure, exchange K1-th and K2-th column of the matrix A.
  */
+function SwapCol($a, $k1, $k2){
+    $temp = 0;
+    for(var $key in  $a ) {
+        var $temp = $a[$key][$k1];
+        $a[$key][$k1] = $a[$key][$k2];
+        $a[$key][$k2] = $temp;
+    }
+
+    return $a;
+}
+$arr = [[2,3,45],[4,6,76],[3,2,1 ]];
+console.log(SwapCol($arr, 0 , 1));
