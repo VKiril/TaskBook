6707f1e430b75f4952e023954bb9cc5be77016d2
asus <everest0corp@gmail.com>
2014-12-05 12:08:18 +0200

    prev date

--- ParamJavascript/62.js
+++ ParamJavascript/62.js
@@ -1,3 +1,37 @@
 /**
- * Created by asus on 03.12.14.
+ * Using the TDate type and the DaysInMonth and CheckDate functions (see Param59−Param61), write a procedure
+ * PrevDate(D) that changes a correct date D (of TDate type) to the previous one; if D contains an invalid date
+ * then it remains unchanged. The parameter D is an input and output parameter. Apply this procedure to five given
+ * dates.
  */
+
+document.write('<scr'+'ipt type="text/javascript" src="61.js" ></scr'+'ipt>');
+
+function PrevDate1(d){
+
+    var checkDate = CheckDate1(d);
+
+    if(checkDate != 0 ){
+        return d;
+    } else {
+        if(d.month == 1 && d.day == 1 ){
+
+
+            d.day = 31;
+            d.month = 12;
+            d.year = d.year - 1;
+        } else if(d.day == 1 && d.month != 1){
+            var prevMonth = new TDate(10, d.month -1 , d.year);
+            var days      = DaysInMonth(prevMonth);
+            d.month       = d.month - 1;
+            d.day         = days[0];
+
+        } else {
+            d.day = d.day - 1;
+        }
+    }
+
+    return d;
+}
+
+$a = new TDate( 1,1,2014);
